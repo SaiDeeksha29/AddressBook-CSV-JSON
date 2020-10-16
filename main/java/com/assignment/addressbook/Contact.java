3501ac4a37de9b8da88fa4fdea80c3ddf55cb282
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-16 21:15:37 +0530

    Added:CSV file to read or write person's contact on address book

---
+++ main/java/com/assignment/addressbook/Contact.java
@@ -0,0 +1,102 @@
+package com.assignment.addressbook;
+/**
+ * @author saideeksha
+ *
+ */
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private long phone;
+	private String email;
+
+	/**
+	 * 
+	 */
+	public Contact(String firstName, String lastName, String address, String city, String state, int zip, long phone,
+			String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		return firstName + " " + lastName + " " + address + " " + city + " " + state + " " + zip + " " + phone + " "
+				+ email;
+	}
+}
